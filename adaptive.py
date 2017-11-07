5ee55dfc0d4aefda6bf5f4f91dd915c8e5b65309
Satyam-agarwal <33454875+Satyam-agarwal@users.noreply.github.com>
2017-11-07 18:47:45 +0530

    Add files via upload
    Run this python code in the directory containing the jpg images and you will get the same enhancement result which we get using Camscanner in Android, improve visualisation , removes uneven brightness in document uses image processing adaption technique
    so without needing android you can get scanned images from your normal jpg images

---
+++ adaptive.py
@@ -0,0 +1,48 @@
+# from skimage import filter
+from skimage import filters
+import scipy.misc
+import Image, numpy
+
+
+
+######################################################################################################3
+#this is code created by me for search image files in directory 
+import os
+import re
+files=os.listdir('./')
+k=[]
+v=[]
+for f in files:
+    
+#    print f[-2:]   #one method of stripping
+     a=f.split('.') # method of spitting
+     if len(a)==2:
+        
+        b=dict(zip(a[0:1],a[-1:])) #creating individual dictionaries
+        k.append(a[0])
+        v.append(a[1])
+
+fin=dict(zip(k,v))    
+
+for i in fin.keys():
+    if fin[i]=='jpg':
+       #print i
+       for m in files:
+           if i in m:
+              print m
+##########################################################################################
+     
+              # opening the image and converting it to grayscale
+              a = Image.open(m).convert('L')
+              # a is converted to an ndarray
+              a = scipy.misc.fromimage(a)
+              # performing adaptive thresholding
+              b = filters.threshold_adaptive(a,51,offset = 10)##yahan par second argument odd hona chahiye even nahi
+
+
+              # b is converted from an ndarray to an image
+              b = scipy.misc.toimage(b) 
+              # saving the image as adaptive_output.png
+              # in the folder Figurespb
+              b.save(m)
+              
